621b07f7b2111326d1c801b0c903b7225c3e9bf5
Ismaël <167563168+Isma-Andri@users.noreply.github.com>
2024-07-12 17:24:07 +0300

    Update tail.c

--- tail.c
+++ tail.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "tail_lib.h"
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Utilisation: %s <nbr_de_ligne> <nom_du_fichier>\n", argv[0]);
-        return 1;
-    }
 
-    int n = atoi(argv[1]);
-    if (n <= 0) {
-        printf("Veuillez entrer un entier positif pour n.\n");
+
+int main(int argc, char *argv[]) {
+    int n = 10;
+    int verbose = 0;
+    char *fichier = NULL;
+
+    optionq(argc, argv, &n, &verbose, &fichier);
+
+    if (fichier == NULL) {
+        printf("Veuillez spécifier un nom de fichier.\n");
         return 1;
     }
 
     FILE *fic = NULL;
-    ouvrir(&fic, argv[2]);
+    ouvrir(&fic, fichier);
     int nbr_ligne = compteur(fic);
     fclose(fic);
 
-    printf("Nombre total de lignes : %d\n", nbr_ligne);
-
-    if (nbr_ligne < n) {
-        printf("Le fichier ne contient que %d lignes, affichage de toutes les lignes.\n", nbr_ligne);
-        n = nbr_ligne; 
-    }
-
-    ouvrir(&fic, argv[2]);
-    tail(fic, nbr_ligne - n, n);
+    ouvrir(&fic, fichier);
+    tail(fic, nbr_ligne - n, n, verbose);
     fclose(fic);
 
     return 0;
