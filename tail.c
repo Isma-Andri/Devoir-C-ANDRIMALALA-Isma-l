7cdf6b6b5eda743b728530c6c3a5a378a5f47984
Ismaël <167563168+Isma-Andri@users.noreply.github.com>
2024-07-16 17:18:52 +0300

    Update tail.c

--- tail.c
+++ tail.c
@@ -3,29 +3,10 @@
 #include <string.h>
 #include "tail_lib.h"
 
-
+#define MAX_CHAR 1000
 
 int main(int argc, char *argv[]) {
-    int n = 10;
-    int verbose = 0;
-    char *fichier = NULL;
-
-    optionq(argc, argv, &n, &verbose, &fichier);
-
-    if (fichier == NULL) {
-        printf("Veuillez spécifier un nom de fichier.\n");
-        return 1;
-    }
-
-    FILE *fic = NULL;
-    ouvrir(&fic, fichier);
-    int nbr_ligne = compteur(fic);
-    fclose(fic);
-
-    ouvrir(&fic, fichier);
-    tail(fic, nbr_ligne - n, n, verbose);
-    fclose(fic);
-
+	FILE* file=NULL;
+	optionManager(file,argv,argc);
     return 0;
 }
-
