148d6ea1eb9c152b945fca784ce0ffc770e033fb
Ismaël <167563168+Isma-Andri@users.noreply.github.com>
2024-07-16 17:20:22 +0300

    Update tail_lib.h

--- tail_lib.h
+++ tail_lib.h
@@ -1,6 +1,2 @@
-#include <stdio.h>
-//#define LONGUEUR_MAX 100
-void ouvrir(FILE **fic, const char *fichier);
-int compteur(FILE *fic);
-void tail(FILE *fic, int ligne_depart, int n, int verbose);
-void optionq(int argc, char *argv[], int *n, int *verbose, char **fichier);
+void tail_default(FILE* file,int argc, char *argv[]);
+void optionManager(FILE* file,char* argv[],int argc);
